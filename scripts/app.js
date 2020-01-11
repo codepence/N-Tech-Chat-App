10d517626d8f373d8e3202e2a0673b5745ebcc38
Lukman Mahmud <mistamann49@gmail.com>
2020-01-11 10:57:51 +0100

    catching and showing user loging and signup errors

---
+++ scripts/app.js
@@ -0,0 +1,139 @@
+// dom queries
+const signupForm = document.querySelector('#signup-form');
+const logout = document.querySelector('#logout');
+const loginForm = document.querySelector('#login-form')
+const chatList = document.querySelector('.chat-list');
+const newChartForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // console.log('user logged in: ', user);
+        setupUI(user);
+    } else {
+        // console.log('user logged out');
+        setupUI();
+    }
+});
+
+// signup
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        // close modal and reset form
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+        signupForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        signupForm.querySelector('.error').innerHTML = err.message;
+    });
+});
+
+// logout
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+// login 
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // close modal and reset form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+        loginForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        loginForm.querySelector('.error').innerHTML = err.message;
+    });
+});
+
+// add a new chat
+newChartForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChartForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChartForm.reset())
+        .catch(err => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    // update name via chatroom
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    // reset the form
+    newNameForm.reset();
+    // show then hide the update message
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+// update the chat room
+rooms.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
+
+// check local storage for a name
+const username = localStorage.username ? localStorage.username : 'Anonymous';
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', username);
+
+// get chats and render
+chatroom.getChats(data => chatUI.render(data));
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function () {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+
+});
+
+const setupUI = user => {
+    if (user) {
+        // account info
+        const html = `
+            <div class="teal">Logged in as ${user.email}</div>
+        `;
+        accountDetails.innerHTML = html;
+        // toggle UI elements
+        loggedInLinks.forEach(item => item.style.display = 'block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+    } else {
+        // hide account info
+        accountDetails.innerHTML = '';
+        // toggle UI elements
+        loggedInLinks.forEach(item => item.style.display = 'none');
+        loggedOutLinks.forEach(item => item.style.display = 'block');
+    }
+};
